7b9ebc3021c388ec6d15e79bf63250957b2369b4
Pau Gonzàlez Martí <lazytime96@gmail.com>
2020-05-22 16:51:09 +0200

    Alineat component Musica

--- resources/js/app.js
+++ resources/js/app.js
@@ -18,19 +18,21 @@ window.Vue = require("vue");
 
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
-
+import { BootstrapVue } from "bootstrap-vue";
 Vue.component(
     "example-component",
     require("./components/ExampleComponent.vue").default
 );
 Vue.component("home", require("./components/Home.vue").default);
 Vue.component("musica", require("./components/Musica.vue").default);
+Vue.component("textos", require("./components/RegistroTextos.vue").default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
+Vue.use(BootstrapVue);
 
 const app = new Vue({
     el: "#app"
